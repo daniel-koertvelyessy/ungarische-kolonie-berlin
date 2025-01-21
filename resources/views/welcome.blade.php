df4c0cd5391495860cfd7fa76e7ef45a46c8111d
Daniel Körtvélyessy <daniel.koertvelyessy@thermo-control.com>
2025-01-21 23:09:57 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,7 +31,7 @@
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                         <div class="flex lg:justify-center lg:col-start-2">
-                            <img src="{{ Vite::asset('./resources/images/header.jpg') }}"
+                            <img src="{{ Vite::asset('resources/images/header.jpg') }}"
                                  alt=""
                             >
                         </div>
