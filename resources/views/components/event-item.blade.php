b1d010e46de4e94d55aadcb88f14dc79b094a63e
Daniel Körtvélyessy <daniel.koertvelyessy@thermo-control.com>
2025-02-11 09:54:06 +0100

    Update event-item.blade.php

--- resources/views/components/event-item.blade.php
+++ resources/views/components/event-item.blade.php
@@ -1,9 +1,9 @@
 <a href="{{ route('events.show',$event->slug[app()->getLocale()]) }}" role="listitem" aria-label="{{ $event->title[app()->getLocale()] }}">
-<flux:card class="space-y-3 my-3 hover:bg-teal-50 dark:hover:bg-emerald-800 transition duration-500 {{ $event->starts_at < now() ? 'border-zinc-600' : 'border-emerald-600' }}">
+<flux:card class="space-y-3 my-3 hover:bg-teal-50 dark:hover:bg-emerald-800 transition duration-500 {{ $event->event_date < now() ? 'border-zinc-600' : 'border-emerald-600' }}">
 
     <flux:heading size="lg">{{ $event->title[app()->getLocale()] }}</flux:heading>
 
-    <flux:text size="sm">{{ __('event.begins') }}: {{ $event->starts_at }} | {{ __('event.ends') }}: {{ $event->ends_at }} | {{ __('event.venue') }}: {{ $event->venue->name }} </flux:text>
+    <flux:text size="sm">{{ __('event.date') }}: {{ $event->event_date->format('Y-m-d') }} | {{ __('event.begins') }}: {{ $event->start_time->format('H:s') }} | {{ __('event.ends') }}: {{ $event->end_time->format('H:s') }} | {{ __('event.venue') }}: {{ $event->venue->name }} </flux:text>
 
 </flux:card>
 </a>
