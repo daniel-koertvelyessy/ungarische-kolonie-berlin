d9fab0f7d01a5a874c3729bf4a4e2aea7f3a0e82
Daniel Körtvélyessy <daniel.koertvelyessy@thermo-control.com>
2025-02-11 09:48:47 +0100

    Update show.blade.php

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -7,7 +7,7 @@
 
 
             @if($event->image)
-                <img src="{{ asset('storage/images/'.$form->event->image) }}"
+                <img src="{{ asset('storage/images/'.$event->image) }}"
                      alt=""
                      class="my-3 lg:my-9 rounded-md shadow"
                 >
