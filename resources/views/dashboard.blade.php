baa156b0345e393203b5b170f90bbb9a537e7f74
Daniel Körtvélyessy <daniel.koertvelyessy@thermo-control.com>
2025-02-11 15:31:48 +0100

    wip
    - change breakpoint for lg and xl on dashboard and transaction->create

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout title="{{ __('nav.dashboard') }}">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-6 lg:gap-9">
+    <div class="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4  gap-3 md:gap-6 lg:gap-9">
         <livewire:dashboard.widgets.applicants />
         <livewire:dashboard.widgets.events />
         <flux:card>
