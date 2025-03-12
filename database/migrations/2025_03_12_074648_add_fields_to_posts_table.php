03c51104ef377d2ce4d5993409e654107f88e49a
Daniel Körtvélyessy <daniel.koertvelyessy@thermo-control.com>
2025-03-12 09:20:39 +0100

    Update 2025_03_12_074648_add_fields_to_posts_table.php

--- database/migrations/2025_03_12_074648_add_fields_to_posts_table.php
+++ database/migrations/2025_03_12_074648_add_fields_to_posts_table.php
@@ -12,18 +12,41 @@ return new class extends Migration {
     {
         Schema::table('posts', function (Blueprint $table)
         {
-            $table->foreignId('post_type_id')
-                ->constrained('post_types');
-            $table->string('label', 255)
-                ->nullable();
-            $table->timestamp('published_at')->nullable();
+            if (!Schema::hasColumn('posts', 'post_type_id')) {
+                $table->foreignId('post_type_id')
+                    ->constrained('post_types');
+            }
+            if (!Schema::hasColumn('posts', 'label')) {
+                $table->string('label', 255)
+                    ->nullable();
+            }
+            if (!Schema::hasColumn('posts', 'published_at')) {
+                $table->timestamp('published_at')
+                    ->nullable();
+            }
         });
 
-        Schema::table('post_images', function (Blueprint $table)
-        {
-            $table->string('author')
-                ->nullable();
-        });
+        if (!Schema::hasTable('post_images')) {
+            Schema::create('post_images', function (Blueprint $table)
+            {
+                $table->id();
+                $table->timestamps();
+                $table->json('caption')
+                    ->nullable();
+                $table->string('filename');
+                $table->string('original_filename');
+                $table->foreignId('post_id')
+                    ->constrained();
+                $table->string('author')
+                    ->nullable();
+            });
+        } else {
+            Schema::table('post_images', function (Blueprint $table)
+            {
+                $table->string('author')
+                    ->nullable();
+            });
+        }
     }
 
 };
