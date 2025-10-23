44d226f8e103ca2cee64c20c8e72777814e751c9
Daniel Körtvélyessy <daniel.koertvelyessy@thermo-control.com>
2025-10-23 21:43:33 +0200

    Update email-to-all-members.blade.php
    fix line break in displayed message

--- resources/views/emails/email-to-all-members.blade.php
+++ resources/views/emails/email-to-all-members.blade.php
@@ -6,12 +6,10 @@
 @if($setPersonalGreeting)
 <h2>{{ __('mails.invitation.greeting',[ 'name' => $mail_name]) }}</h2><br>
 @endif
-<p>{{ $mail_message }}</p>
+<p>{!! nl2br(e($mail_message)) !!}</p>
 
 @if($setLink && $url !=='')
     <x-mails.link-button href="{{ $url }}" >{{ $url_label }}</x-mails.link-button>
 @endif
-
-
 
 <x-mails.footer/>
